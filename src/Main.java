c6edb1adebdbc9dd5b12b5449fccb1bb353bc62a
David <david.ammon@student.htldornbirn.at>
2023-10-11 11:06:44 +0200

    Readme File erstellt

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // short - kurze Zahl
+        // int - ganze Zahl
+        // float - Kommazahl
+        // double: kommazahl (groß)
+        // boolean - true/false
+        // char - Zeichen
+        // string - Zeichenkette
+
+        int number = 7;
+        string_name = "Andi Wand";
+    }
+
+//strg blank
+}
