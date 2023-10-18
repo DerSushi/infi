8a9b672e1c9cbe264286c9741f8a13b5762c2051
David <david.ammon@student.htldornbirn.at>
2023-10-18 11:02:58 +0200

    Uebung 2 abgeschlossen.

--- src/at/ada/basis/ControlStructure4.java
+++ src/at/ada/basis/ControlStructure4.java
@@ -22,18 +22,12 @@ public class ControlStructure4 {
         }
         // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
         // dann gib aus "Eine der beiden ist kleiner als 30"
-        if (randomNumber<30)
-        {
-            System.out.println("Zahl 1 ist kleiner als 30.");
-        }
-        if (randomNumber2<30)
-        {
-            System.out.println("Zahl 2 ist kleiner als 30.");
+        if (randomNumber<30 || randomNumber2<30) {
+            System.out.println("Eine Zahl ist kleiner als 30.");
         }
         // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
         // dann gib aus "Erste Zahl klein, zweite kein 50iger"
-        if (randomNumber<50)
-        {
+        if (randomNumber<50) {
             if (randomNumber2 != 50) {
                 System.out.println("Die erste Zahl ist klein und die zweite nicht 50.");
             }
