707f1de7c89e75ff8df314f345878d18c9aeccdd
David <david.ammon@student.htldornbirn.at>
2023-11-15 11:04:19 +0100

    Kontostand überprüfung, einzahlung und abheben und beenden fertig programmiert.

--- src/at/ada/basis/BankomatBeispiel.java
+++ src/at/ada/basis/BankomatBeispiel.java
@@ -7,54 +7,81 @@ import javax.swing.JOptionPane;
 public class BankomatBeispiel {
     public static void main(String[] args) {
         float Bankgeldf = 0;
-        float Eingabef = 0;
-        for(int i=1; i>0; i++){
-        Scanner scanner = new Scanner(System.in);
-                    try {
-                       Thread.sleep(2 * 1000);
-                      } catch (InterruptedException ie) {
-                        Thread.currentThread().interrupt();
-                        }
-                    System.out.println("Wählen Sie einen Befehl aus:");
-                    System.out.println("1. (e) für Geld einzahlen");
-                    System.out.println("2. (a) für Geld abheben");
-                    System.out.println("3. (k) für Kontostand prüfen");
-                    System.out.println("4. (b) für Interaktion beenden");
-                    char AuswahlC = scanner.next().charAt(0);
-                    switch(AuswahlC){
+        float eingabef = 0;
+        String fNr = "";
+        for (int i = 1; i > 0; i++) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                Thread.sleep(2 * 1000);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Wählen Sie einen Befehl aus:");
+            System.out.println("1. (e) für Geld einzahlen");
+            System.out.println("2. (a) für Geld abheben");
+            System.out.println("3. (k) für Kontostand prüfen");
+            System.out.println("4. (b) für Interaktion beenden");
+            char AuswahlC = scanner.next().charAt(0);
+            switch (AuswahlC) {
                 case 'e':
                     System.out.println("Wie viel Geld wollen sie einzahlen?");
-                    try {
-                        Eingabef = scanner.nextFloat();
-                        if (Eingabef > 0) {
-                            Bankgeldf += Eingabef;
-                            System.out.println("Einzahlung von " + Eingabef + " Euro erfolgreich. Neuer Kontostand: " + Bankgeldf + " Euro");
-                        } else {
-                            System.out.println("Ungültiger Einzahlungsbetrag. Bitte geben Sie einen positiven Betrag ein.");
-                        }
+                    fNr = scanner.next();
+                    if (checkFloat(fNr)) {
+                        eingabef = Float.parseFloat(fNr);
+                    } else {
+                        eingabef = 0;
                     }
-                    } catch (NumberFormatException Eingabef) {
-                System.out.println("Ihre Angabe ist nicht valide.");
-            }
+
+                    if (eingabef > 0) {
+                        Bankgeldf += eingabef;
+                        System.out.println("Einzahlung von " + eingabef + " Euro erfolgreich. Neuer Kontostand: " + Bankgeldf + " Euro");
+                    } else {
+                        System.out.println("Ungültiger Einzahlungsbetrag. Bitte geben Sie einen positiven Betrag ein.");
+                    }
+
                     break;
                 case 'a':
                     System.out.println("Wie viel Geld wollen sie abheben?");
-                    Eingabef = scanner.nextFloat();
-                    if (Eingabef > 0) {
-                        Bankgeldf -= Eingabef;
-                        System.out.println("Einzahlung von " + Eingabef + " Euro erfolgreich. Neuer Kontostand: " + Bankgeldf + " Euro");
+                    fNr = scanner.next();
+                    if (checkFloat(fNr)) {
+                        eingabef = Float.parseFloat(fNr);
+                    } else {
+                        eingabef = 0;
                     }
-                    else {
+                    //eingabef = scanner.nextFloat();
+                    if (eingabef > 0) {
+                        Bankgeldf -= eingabef;
+                        System.out.println("Einzahlung von " + eingabef + " Euro erfolgreich. Neuer Kontostand: " + Bankgeldf + " Euro");
+                    } else {
                         System.out.println("Sie können kein negatives Geld abheben!");
                     }
                     break;
                 case 'k':
-                    System.out.println("Ihr Konto beinhaltet: "+Bankgeldf+" Euro");
+                    System.out.println("Ihr Konto beinhaltet: " + Bankgeldf + " Euro");
                     break;
                 case 'b':
                     System.out.println("Vielen Dank, dass Sie heute mit uns interagiert haben.");
                     System.exit(0);
             }
+        }
+    }
+
+    public static Float getInput(String eingabetext, Scanner scanner) {
+        Float eingabef =0.0F;
+        String fNr = "";
+        System.out.println(eingabetext);
+        fNr = scanner.next();
+        if (checkFloat(fNr)) {
+            eingabef = Float.parseFloat(fNr);
+        }
+        return eingabef;
+    }
+    public static Boolean checkFloat(String s) {
+        try{
+            Float.parseFloat(s);
+            return true;
+        } catch (NumberFormatException error) {
+            return false;
         }
     }
 }
